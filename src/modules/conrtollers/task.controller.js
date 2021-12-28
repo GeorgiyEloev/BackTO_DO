af35ae493588577e5db8f844088a759902795afa
GeorgiyEloev <geloev.exceedteam@gmail.com>
2021-12-28 14:54:05 +0300

    fix

--- src/modules/conrtollers/task.controller.js
+++ src/modules/conrtollers/task.controller.js
@@ -7,15 +7,15 @@ module.exports.getAllTasks = (req, res) => {
 };
 
 module.exports.getOneTask = (req, res) => {
-	const id = req.query._id;
-	if (id) {
-		Task.findOne({ _id: id }).then((result) => {
-			res.send({ data: result });
-		})
-	}  else {
+  const id = req.query._id;
+  if (id) {
+    Task.findOne({ _id: id }).then((result) => {
+      res.send({ data: result });
+    });
+  } else {
     res.status(422).send("Error! Неверные параметры!");
   }
-}
+};
 
 module.exports.createNewTask = (req, res) => {
   if (
